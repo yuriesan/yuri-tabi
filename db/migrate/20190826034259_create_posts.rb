f8ccd9f2bf1e1ec9decd48dda021cb8088608eb4
yuriesan <harayuri0407@gmail.com>
2019-08-28 10:32:50 +0900

    edit migrationfile/model

--- db/migrate/20190826034259_create_posts.rb
+++ db/migrate/20190826034259_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.text :text , null: false
+      t.string :image
+      t.integer :user_id, null: false
 
       t.timestamps
     end
--- db/migrate/20190826034311_create_comments.rb
+++ db/migrate/20190826034311_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
+      t.integer :user_id , null: false
+      t.integer :post_id , null: false
+      t.text :text , null: false
       t.timestamps
     end
   end
